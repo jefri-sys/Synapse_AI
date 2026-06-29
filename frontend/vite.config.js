fad492291989aa46d0ae7965c5c2ef23d2308043
jefri-sys <jefrijiji2003@gmail.com>
2026-06-29 18:26:00 +0530

    fix: revert outDir in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -145,8 +145,6 @@ export default defineConfig({
     include: ['react-dropzone', 'tslib']
   },
   build: {
-    outDir: process.env.VERCEL ? '../dist' : 'dist',
-    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks(id) {
